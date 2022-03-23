5abf702b18791d2276f2a932b86cb0b0ce58d1f2
Matias Nisula <matiasnisula81@gmail.com>
2022-03-23 12:21:19 +0200

    style fixes test_statistics.py

--- viikko1/nhl-statistics-1/src/tests/test_statistics.py
+++ viikko1/nhl-statistics-1/src/tests/test_statistics.py
@@ -19,32 +19,32 @@ class TestStatistics(unittest.TestCase):
         )
 
     def test_search_returns_player_if_exits(self):
-        self.player = self.statistics.search("Kurri")
+        player = self.statistics.search("Kurri")
 
-        self.assertEqual(self.player.name, "Kurri")
+        self.assertEqual(player.name, "Kurri")
 
     def test_search_returns_none_if_player_doesnt_exits(self):
-        self.player = self.statistics.search("Litmanen")
+        player = self.statistics.search("Litmanen")
 
-        self.assertEqual(self.player, None)
+        self.assertEqual(player, None)
 
     def test_team_returns_correct_players(self):
-        self.team = self.statistics.team("EDM")
+        team = self.statistics.team("EDM")
         player_list = ["Semenko", "Kurri", "Gretzky"]
 
-        for player in self.team:
+        for player in team:
             if player.name in player_list:
                 player_list.remove(player.name)
 
         self.assertEqual(len(player_list), 0)
 
     def test_top_scorers_returns_players_in_correct_order(self):
-        self.top_5 = self.statistics.top_scorers(5)
+        top_5 = self.statistics.top_scorers(5)
         correct_top_5 = ["Gretzky", "Lemieux", "Yzerman", "Kurri", "Semenko"]
         correct_order = True
 
-        for i in range(len(self.top_5)):
-            if correct_top_5[i] != self.top_5[i].name:
+        for i in range(len(top_5)):
+            if correct_top_5[i] != top_5[i].name:
                 correct_order = False
 
         self.assertEqual(correct_order, True)
