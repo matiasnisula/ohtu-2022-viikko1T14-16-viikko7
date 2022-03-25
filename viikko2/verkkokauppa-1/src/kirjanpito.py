2b9e1e9a2c2720e8c00dd7f737d03c3c83915476
Matias Nisula <matiasnisula81@gmail.com>
2022-03-25 13:52:32 +0200

    Teht 10-12 vko 2 verkkokauppa-1

---
+++ viikko2/verkkokauppa-1/src/kirjanpito.py
@@ -0,0 +1,12 @@
+class Kirjanpito:
+
+    def __init__(self):
+        self.tapahtumat = []
+
+    def lisaa_tapahtuma(self, tapahtuma):
+        self.tapahtumat.append(tapahtuma)
+
+    def get_tapahtumat(self):
+        return self.tapahtumat
+
+kirjanpito = Kirjanpito()
