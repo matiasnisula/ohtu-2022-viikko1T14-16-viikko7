697a144c34c94b10521bfe0914f20c7e0a139e09
Matias Nisula <matiasnisula81@gmail.com>
2022-04-29 10:12:38 +0300

    viikko6 T4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,20 +1,23 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan, Not, Or
+from query_builder import QueryBuilder
 
 def main():
     url = "https://nhlstatisticsforohtu.herokuapp.com/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-    HasAtLeast(40, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("NYI"),
-        PlaysIn("BOS")
+    query = QueryBuilder()
+    matcher = (
+      query  
+        .playsIn("NYR")  
+        .hasAtLeast(5, "goals")  
+        .hasFewerThan(10, "goals")  
+        .build()
     )
-)
+
+
     for player in stats.matches(matcher):
         print(player)
 
---
+++ viikko6/query-language/src/query_builder.py
@@ -0,0 +1,18 @@
+from matchers import And, HasAtLeast, PlaysIn, All, HasFewerThan, Not, Or
+
+class QueryBuilder:
+    def __init__(self, query = All()):
+        self._query = query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(PlaysIn(team), self._query))
+
+    def hasAtLeast(self, value, attr):
+        return QueryBuilder(And(HasAtLeast(value, attr), self._query))
+
+    def hasFewerThan(self, value, attr):
+        return QueryBuilder(And(HasFewerThan(value, attr), self._query))
+
+
+    def build(self):
+        return self._query
